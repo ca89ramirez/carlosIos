625ac8cb14927e40c36703b5a2b25a0b1c63c9f1
Carlos Ramirez <cramirez@ozelot.it>
2016-04-21 13:56:02 -0500

    comocm

--- HttpCarlos/ViewController.swift
+++ HttpCarlos/ViewController.swift
@@ -7,36 +7,67 @@
 //
 
 import UIKit
+import SystemConfiguration
+
 
 class ViewController: UIViewController {
     
     @IBOutlet weak var imagen: UIImageView!
-    
     @IBOutlet weak var libro: UILabel!
-    
     @IBOutlet weak var autor: UILabel!
-    
     @IBOutlet weak var portada: UILabel!
-    
     @IBOutlet weak var textoISB: UITextField!
-    
     @IBAction func limparCampos(sender: AnyObject) {
         textArea.text = " "
         textoISB.text = " "
         libro.text = " "
         autor.text = " "
         portada.text = " "
+    }
+    
+    @IBOutlet weak var textArea: UITextView!
+       @IBAction func BuscarButton(sender: UIButton) {
+        
+        print("Hello world")
+        if isConnectedToNetwork() == true {
+            print("Internet connection OK")
+            sincrono()
+        } else {
+            print("Internet connection FAILED")
+        }
+    
+        
+        print("bye")
+        
         
     }
-    @IBOutlet weak var textArea: UITextView!
-    @IBAction func BuscarButton(sender: UIButton) {
-        print("Hello world")
-        sincrono()
-        print("bye")
+
+    func isConnectedToNetwork() -> Bool {
+        var zeroAddress = sockaddr_in()
+        zeroAddress.sin_len = UInt8(sizeofValue(zeroAddress))
+        zeroAddress.sin_family = sa_family_t(AF_INET)
+        let defaultRouteReachability = withUnsafePointer(&zeroAddress) { SCNetworkReachabilityCreateWithAddress(nil, UnsafePointer($0)) }
+        var flags = SCNetworkReachabilityFlags()
+        if !SCNetworkReachabilityGetFlags(defaultRouteReachability!, &flags) {
+            return false
+        }
+        let isReachable = (flags.rawValue & UInt32(kSCNetworkFlagsReachable)) != 0
+        let needsConnection = (flags.rawValue & UInt32(kSCNetworkFlagsConnectionRequired)) != 0
+        return (isReachable && !needsConnection)
     }
+    
     func sincrono(){
         var ISB = String()
         ISB = textoISB.text!
+        
+      
+    
+        
+        if ISB.isEmpty{
+            print("-Estado vacio-----")
+            libro.text = "Ingrese un ISBN"
+        }else{
+        
         print(ISB)
         print("-------")
         let urls = "https://openlibrary.org/api/books?jscmd=data&format=json&bibkeys=ISBN:\(ISB)"
@@ -44,111 +75,52 @@ class ViewController: UIViewController {
     //    let urls="https://openlibrary.org/api/books?jscmd=data&format=json&bibkeys=ISBN:9788437604947"
         print("-------")
         let url=NSURL(string: urls)
-        
         let datos : NSData? =  NSData(contentsOfURL:url!)
-        
-        do{
-        let json = try NSJSONSerialization.JSONObjectWithData(datos!, options: NSJSONReadingOptions.MutableLeaves)
-        let dico1 = json as! NSDictionary
-        let dico2 = dico1["ISBN:9788437604947"] as! NSDictionary
-            
-            
-            let title = dico2["title"] as? String
-            libro.text = title
-            print ("mi titulo \(title)")
-            
-       /* let dico3 = dico2["publishers"] as? [[String : AnyObject]]
-            if let publishers = dico3["publishers"] as? [AnyObject]{
-            for nombre in publishers{
-                
-                }
-            }*/
-            let dico3 = dico2 ["publishers"] as! [[String:AnyObject]]
-            
-            for publish in dico3 {
-                let publicacion = publish["name"] as? String
-                print("mi pub \(publicacion)")
-                portada.text = publicacion
-                
-                
-            }
-            
-            
-            
-            
-      
-            let name = dico3.first
-            print("-----")
-           print(name)
-            print("-----")
-            
-            let dico4 = dico2["authors"] as! [[String:AnyObject]]
-            let auto = dico4.first
-            print("-----")
-            print(auto)
-            print("-----")
-            
-            for autoor in dico4 {
-                let author = autoor["name"] as? String
-                print("mi autor \(author)")
-                autor.text = author
-                
-                
-            }
-           
-            let imageURL = NSURL(string: "http://covers.openlibrary.org/b/isbn/\(ISB)-L.jpg")!
-            let image = NSData(contentsOfURL: imageURL)!
-            imagen.image = UIImage(data: image)
+        let comp = NSString (data: datos!, encoding: NSUTF8StringEncoding)
+        if comp == "{}" {
+            print("-Código Erroneo--")
+            libro.text = "No existe el ISBN "
 
-            
-           // dico4= dico3["pagination"] as! NSString as String
-            
-            //let dico5 =
-            
-          //  autor.text = dico3["pagination"] as! NSString as! String
-            
-            
-            
-        
-            
-            
-            
-            
-            
-            
-            
-            var names : String = " "
-            
-            do {
-                let json2 = try NSJSONSerialization.JSONObjectWithData(datos!, options: .AllowFragments)
-
-                if let publishers = json2["publishers"] as? [[String: AnyObject]] {
-                    for nam in publishers {
-                        if let name = nam["name"] as? String {
-                            names=name
-                        }
-                    }
+        }else{
+                        do{
+                let json = try NSJSONSerialization.JSONObjectWithData(datos!, options: NSJSONReadingOptions.MutableLeaves)
+                let dico1 = json as! NSDictionary
+                let dico2 = dico1["ISBN:\(ISB)"] as! NSDictionary
+                let title = dico2["title"] as? String
+                libro.text = title
+                print ("mi titulo \(title)")
+                /* let dico3 = dico2["publishers"] as? [[String : AnyObject]]
+                if let publishers = dico3["publishers"] as? [AnyObject]{
+                for nombre in publishers{
                 }
-            } catch {
-                print("error serializing JSON: \(error)")
+                }*/
+                let dico3 = dico2 ["publishers"] as! [[String:AnyObject]]
+                for publish in dico3 {
+                    let publicacion = publish["name"] as? String
+                    print("mi pub \(publicacion)")
+                    portada.text = publicacion   }
+                let name = dico3.first
+                print("-----")
+                print(name)
+                print("-----")
+                let dico4 = dico2["authors"] as! [[String:AnyObject]]
+                let auto = dico4.first
+                print("-----")
+                print(auto)
+                print("-----")
+                for autoor in dico4 {
+                    let author = autoor["name"] as? String
+                    print("mi autor \(author)")
+                    autor.text = author
+                }
+                let imageURL = NSURL(string: "http://covers.openlibrary.org/b/isbn/\(ISB)-L.jpg")!
+                let image = NSData(contentsOfURL: imageURL)!
+                imagen.image = UIImage(data: image)
             }
-            
-            print("c \(names)" )
-           // autor.text = names as! String
-            
-            
-            
-          //  autor.text = dico3["name"] as! NSString as String
-        
-            
-        
-        }
-        catch _{
-            
-        }
-        
-        
-        if datos != nil{
+            catch _{
+            }
+            }}
+     /*   if datos != nil{
             print("no nulo")
             let texto = NSString(data:datos!, encoding:NSUTF8StringEncoding)
             textArea.text = texto as! String
@@ -160,24 +132,19 @@ class ViewController: UIViewController {
         }
         else{
             textArea.text = "No se cuenta con acceso a Internet"
-        }
-       // 9788437604947
+        }*/
          }
-    
-    
-    
-
     override func viewDidLoad() {
         super.viewDidLoad()
         
         // Do any additional setup after loading the view, typically from a nib.
     }
-
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
+ 
 
 }
 
